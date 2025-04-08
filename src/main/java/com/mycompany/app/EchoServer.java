22ec7d566a87d89bf271b7ad9326690ac67405e5
I539230 <luis.maier@sap.com>
2025-04-08 18:20:29 +0200

    Stop the EchoServer

--- src/main/java/com/mycompany/app/EchoServer.java
+++ src/main/java/com/mycompany/app/EchoServer.java
@@ -12,9 +12,17 @@ public class EchoServer {
                 try (Socket connection = server.accept()){ //accept any new connection
                     //Here is defined what the server should do
                     in = new BufferedReader(new InputStreamReader(connection.getInputStream())); //The request gets saved
-
-                    PrintWriter out = new PrintWriter(connection.getOutputStream()); //The response gets prepared
-                    out.println(in.readLine()); //the value of in gets appended to the response
+                    PrintWriter out = new PrintWriter(connection.getOutputStream()); //The response object gets prepared
+                    String line = in.readLine();
+                    if (line.equals("stop!")) { //If the request is stop! the server stops
+                        System.out.println("Server STOP!");
+                        out.println("Bye Bye");
+                        out.flush();
+                        server.close();
+                        break;
+                    } 
+                    
+                    out.println(line); //the value of in gets appended to the response
                     out.flush(); //the reponse gets send
                 } catch (Exception e) {
                     System.err.println(e);
